7aa20b6f3946a43de4c60ef8a97daa11acbcb841
Rushy Panchal <panchal.rushy@gmail.com>
2015-04-19 02:06:06 -0400

    Add locations in the appropriate map areas.

--- js/load-map.js
+++ js/load-map.js
@@ -1,6 +1,55 @@
 var stateInfo = {
+	AL: ["Alabama"],
+	AK: ["Alaska", "Has one of the largest deposits of oil"],
+	AZ: ["Arizona"],
+	AR: ["Arkansas"],
 	CA: ["California"],
-	NJ: ["New Jersey"]
+	CO: ["Colorado", "Coal mining, especially in the Denver basin"],
+	CT: ["Connecticut"],
+	DC: ["District of Columbia"],
+	DE: ["Delaware"],
+	FL: ["Florida"],
+	GA: ["Georgia"],
+	HI: ["Hawaii"],
+	ID: ["Idaho"],
+	IL: ["Illinois", "Recoverable Coal Reserve", "Harrisburg Coal Field"],
+	IN: ["Indiana"],
+	IA: ["Iowa"],
+	KS: ["Kansas"],
+	KY: ["Kentucky", "Coal mining"],
+	LA: ["Louisiana", "One of the top producers of natural gas"],
+	ME: ["Maine"],
+	MD: ["Maryland"],
+	MA: ["Massachusetts"],
+	MI: ["Michigan"],
+	MN: ["Minnesota"],
+	MS: ["MIssissippi"],
+	MO: ["MIssouri"],
+	MT: ["Montana", "Recoverable Coal Reserve", "Oil-bearing shales"],
+	NE: ["Nebraska"],
+	NV: ["Nevada"],
+	NH: ["New Hampshire"],
+	NJ: ["New Jersey"],
+	NM: ["New Mexico", "One of the top producers of natural gas"],
+	NY: ["New York"],
+	NC: ["North Carolina"],
+	ND: ["North Dakota", "Contains oil-bearing shales"],
+	OH: ["Ohio"],
+	OK: ["Oklahoma", "One of the top producers of natural gas"],
+	OR: ["Oregon"],
+	PA: ["Pennsylvania", "One of the largest deposits of anthracite coal"],
+	RI: ["Rhode Island"],
+	SC: ["South Carolina"],
+	SD: ["South Dakota"],
+	TN: ["Tennessee"],
+	TX: ["Texas", "One of the top producers of natural gas"],
+	UT: ["Utah"],
+	VT: ["Vermont"],
+	VA: ["Virginia"],
+	WA: ["Washington"],
+	WV: ["West Virginia", "Recoverable Coal Reserve", "Significant coal mining"],
+	WI: ["Wisconsin"],
+	WY: ["Wyoming", "Coal mined in the Powder River Basin", "Recoverable Coal Reserve", "Largest coal-producing site", "One of the top producers of natural gas"]
 	};
 
 function displayStateInfo(state) {
@@ -29,7 +78,10 @@ $(document).ready(function() {
 			}
 		});
 
-	var stateInfoElem = $("#state-info").get(0);
+	var stateInfoElem = $("#state-info"),
+		stateInfoAll = $("#state-info-all");
+
+	stateInfoAll.css("display", "none");
 
 	for (var state in stateInfo) {
 		var elem = document.createElement("div"),
@@ -39,8 +91,11 @@ $(document).ready(function() {
 
 		header.innerHTML = specificStateInfo[0];
 		elem.id = state;
-		elem.appendChild(items);
 		elem.appendChild(header);
+		elem.appendChild(items);
+
+		// display all sources of coal, oil, and gas in one spot, in drop-down
+		// check if coal, oil, or gas by checking if string contains (i.e "gas" in elem)
 
 		for (var index in specificStateInfo) {
 			if (index == 0) {
@@ -51,6 +106,23 @@ $(document).ready(function() {
 			items.appendChild(item);
 			}
 
-		stateInfoElem.appendChild(elem);
+		stateInfoElem.append(elem);
+		if (specificStateInfo.length > 1) {
+			stateInfoAll.append(elem);
+			}
 		}
+
+	$("#toggle-state-all").click(function() {
+		var button = $(this);
+		$("#state-info-all").slideToggle(function() {
+			if (button.hasClass("fa-angle-up")) {
+				button.removeClass("fa-angle-up");
+				button.addClass("fa-angle-down");
+				}
+			else {
+				button.addClass("fa-angle-up");
+				button.removeClass("fa-angle-down");
+				}
+			});
+		});
 	});
